adb5bc455dd331319f380fc81257a2cd67d1753f
AgniaEndie <endienasg2@gmail.com>
2025-02-13 21:50:28 +0500

    initial commit for networking

---
+++ Core/Inc/esp8266.h
@@ -0,0 +1,8 @@
+//
+// Created by Endie on 13.02.2025.
+//
+
+#ifndef ESP8266_H
+#define ESP8266_H
+
+#endif //ESP8266_H
---
+++ Core/Src/esp8266.c
@@ -0,0 +1,5 @@
+//
+// Created by Endie on 13.02.2025.
+//
+
+#include "esp8266.h"
