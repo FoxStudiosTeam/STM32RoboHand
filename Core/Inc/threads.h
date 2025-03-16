7637411132c6dcd80a21d686a11e6dd6ba3b642c
AgniaEndie <endienasg2@gmail.com>
2025-03-16 16:17:54 +0500

    setup for abstract network management of esp01 8266

--- Core/Inc/threads.h
+++ Core/Inc/threads.h
@@ -8,6 +8,8 @@
 #include <stdio.h>
 #include <messages.h>
 
+#include "esp8266.h"
+
 static void MainDisplayMessagesThread(void const *argument) {
     ssd1306_Init();
     HelloMessage();
@@ -18,7 +20,7 @@ static void LedTaskThread(void const *argument) {
     void **args = (void **) argument;
 
     GPIO_TypeDef *ledPinGroup = (GPIO_TypeDef *) args[0];
-    uint16_t ledPin = (uint16_t)(uintptr_t)args[1];
+    uint16_t ledPin = (uint16_t) (uintptr_t) args[1];
 
     while (1) {
         HAL_Delay(500);
@@ -27,4 +29,14 @@ static void LedTaskThread(void const *argument) {
         HAL_GPIO_WritePin(ledPinGroup, ledPin, GPIO_PIN_RESET);
     }
 }
+
+static void NetworkTaskThread(void const *argument) {
+    void **args = (void **) argument;
+
+    ESP_8266 esp8266 = {};
+    ESP_8266 *espPtr = &esp8266;
+
+    setup(espPtr);
+}
+
 #endif //THREADS_H
--- Core/Src/esp8266.c
+++ Core/Src/esp8266.c
@@ -17,7 +17,7 @@ void setup(ESP_8266 *esp8266) {
 
   ESP_8266_GPIO_HELPER_STRUCT b15 = {
        .group = GPIOB,
-       .pin = GPIO_PIN_13,
+       .pin = GPIO_PIN_15,
    };
 
   ESP_8266 local = {
